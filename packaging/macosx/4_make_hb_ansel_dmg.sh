185796765a5d0f7073f278b7654408723e2fbb42
Marrony Neris <neris.marrony@gmail.com>
2024-11-12 11:25:50 -0800

    minors

--- packaging/macosx/4_make_hb_ansel_dmg.sh
+++ packaging/macosx/4_make_hb_ansel_dmg.sh
@@ -47,7 +47,7 @@ echo '
 chmod -Rf go-w /Volumes/"${PROGN}"
 sync
 hdiutil detach ${device}
-DMG="${PROGN}-$(git describe --tags | sed 's/^release-//;s/-/+/;s/-/~/;s/rc/~rc/')-$(arch)"
+DMG="ansel-${VERSION:-nightly}-$(arch)"
 hdiutil convert "pack.temp.dmg" -format UDZO -imagekey zlib-level=9 -o "${DMG}"
 rm -f pack.temp.dmg
 
